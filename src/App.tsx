abc061f052e98e7013e6de3ec1ca6b90d874a35a
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-12-18 15:43:49 +0100

    build: test circle-ci build

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App(): JSX.Element {
     <div className='App'>
       <header className='App-header'>
         <img alt='logo' className='App-logo' src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Test circle-ci</p>
         <a
           className='App-link'
           href='https://reactjs.org'
