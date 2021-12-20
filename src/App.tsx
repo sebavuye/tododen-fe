c7bf8a3413c83161d4b353a4d486973763bf0beb
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-12-20 20:50:35 +0100

    feat: add colors & status bar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import { Grid, GridItem, Heading } from '@chakra-ui/react';
+import { Badge, Grid, GridItem, Heading, Text } from '@chakra-ui/react';
 
 function App(): JSX.Element {
   return (
     <main>
-      <Grid h='100vh' templateColumns='4'>
+      <Grid bg='teal.900' h='100vh' templateColumns='4'>
         <Grid mx='auto' templateRows='auto 1fr auto' w='90%'>
-          <GridItem p={4} textAlign='center'>
-            <Heading as='h1' size='2xl'>
+          <GridItem bg='teal.900' p={4} textAlign='center'>
+            <Heading as='h1' color='white' size='2xl'>
               TodoDen
             </Heading>
           </GridItem>
 
-          <GridItem bg='green.500'>
-            <Heading as='h1' size='2xl'>
-              Content
-            </Heading>
+          <GridItem>
+            <Grid h='100%' templateRows='auto 1fr'>
+              <GridItem
+                alignItems='end'
+                bg='teal.100'
+                display='flex'
+                justifyContent='end'
+                p={4}>
+                <Badge colorScheme='pink' mx={1}>
+                  To Do: 0
+                </Badge>
+                <Badge colorScheme='yellow' mx={1}>
+                  Done: 0
+                </Badge>
+              </GridItem>
+              <GridItem bg='teal.50'>
+                <Text as='p'>To Do List</Text>
+              </GridItem>
+            </Grid>
           </GridItem>
 
           <GridItem p={4} textAlign='center'>
-            <Heading as='h1' size='2xl'>
+            <Heading as='h1' size='2xl' color='white'>
               Footer
             </Heading>
           </GridItem>
