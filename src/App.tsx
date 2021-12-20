da154cf41527ddf14d2339a071e0fda0c266a481
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-12-20 21:58:16 +0100

    feat: add list layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-/* eslint-disable */
 import React from 'react';
 import {
   Badge,
   Button,
+  Flex,
   Grid,
   GridItem,
   Heading,
+  Icon,
   Input,
-  InputGroup
+  InputGroup,
+  List,
+  ListItem,
+  Text
 } from '@chakra-ui/react';
+import {
+  IoCloseCircleOutline,
+  IoEllipseOutline,
+  IoPencilOutline
+} from 'react-icons/io5';
 
 function App(): JSX.Element {
   return (
@@ -39,7 +48,41 @@ function App(): JSX.Element {
 
               <GridItem bg='teal.50' p={6}>
                 <Grid h='100%' mx='auto' templateRows='1fr auto' w='90%'>
-                  <GridItem>Test</GridItem>
+                  <GridItem>
+                    <List spacing={4}>
+                      <ListItem>
+                        <Flex justifyContent='space-between'>
+                          <Flex flex={1}>
+                            <Icon
+                              w={6}
+                              h={6}
+                              as={IoEllipseOutline}
+                              color='gray.400'
+                            />
+                            <Text pl={2} as='span'>
+                              To Do 1
+                            </Text>
+                          </Flex>
+                          <Flex justifyContent='end'>
+                            <Icon
+                              w={6}
+                              h={6}
+                              as={IoPencilOutline}
+                              color='gray.400'
+                              mx={1}
+                            />
+                            <Icon
+                              w={6}
+                              h={6}
+                              as={IoCloseCircleOutline}
+                              color='gray.400'
+                              ml={1}
+                            />
+                          </Flex>
+                        </Flex>
+                      </ListItem>
+                    </List>
+                  </GridItem>
                   <GridItem display='flex'>
                     <InputGroup size='lg'>
                       <Input placeholder='What needs to be done?' />
