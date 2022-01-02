6e956991e4f57ba044065f7756a3fcf78d3f5196
Sebastian Vuye <sebastian.vuye@gmail.com>
2022-01-02 16:42:45 +0100

    feat: add 'focus' on input

--- src/components/toDoList/ToDoListItem.tsx
+++ src/components/toDoList/ToDoListItem.tsx
@@ -15,58 +15,85 @@ const ToDoListItem = ({
   onDelete,
   onEdit,
   onUpdate
-}: ToDoListItemProps): JSX.Element => (
-  <ListItem>
-    <Flex justifyContent='space-between'>
-      <Flex alignItems='center' flex={1}>
-        {completed ? (
+}: ToDoListItemProps): JSX.Element => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (!editMode) return;
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editMode]);
+
+  return (
+    <ListItem>
+      <Flex justifyContent='space-between'>
+        <Flex alignItems='center' flex={1}>
+          {completed ? (
+            <Icon
+              as={IoCheckmarkCircleSharp}
+              color='teal.400'
+              cursor='pointer'
+              h={6}
+              w={6}
+              onClick={onUpdate}
+            />
+          ) : (
+            <Icon
+              as={IoEllipseOutline}
+              color='gray.400'
+              cursor='pointer'
+              h={6}
+              w={6}
+              onClick={onUpdate}
+            />
+          )}
+          {editMode ? (
+            <Input
+              ref={inputRef}
+              defaultValue={children as string}
+              size='sm'
+              type='text'
+              onBlur={event => {
+                onEdit(event.target.value);
+              }}
+            />
+          ) : (
+            <Text
+              as='span'
+              pl={2}
+              onClick={() => {
+                onEdit(children as string);
+              }}>
+              {completed ? <Text as='s'>{children}</Text> : children}
+            </Text>
+          )}
+        </Flex>
+        <Flex alignItems='center' justifyContent='end'>
           <Icon
-            as={IoCheckmarkCircleSharp}
-            color='teal.400'
-            cursor='pointer'
-            h={6}
-            w={6}
-            onClick={onUpdate}
-          />
-        ) : (
-          <Icon
-            as={IoEllipseOutline}
+            as={IoPencilOutline}
             color='gray.400'
             cursor='pointer'
             h={6}
+            mx={1}
             w={6}
-            onClick={onUpdate}
-          />
-        )}
-        {editMode ? (
-          <Input
-            defaultValue={children as string}
-            size='sm'
-            type='text'
-            onBlur={event => {
-              onEdit(event.target.value);
+            onClick={() => {
+              onEdit(children as string);
             }}
           />
-        ) : (
-          <Text as='span' pl={2} onClick={() => onEdit(children as string)}>
-            {completed ? <Text as='s'>{children}</Text> : children}
-          </Text>
-        )}
+          <Icon
+            as={IoCloseCircleOutline}
+            color='gray.400'
+            cursor='pointer'
+            h={6}
+            ml={1}
+            w={6}
+            onClick={onDelete}
+          />
+        </Flex>
       </Flex>
-      <Flex justifyContent='end'>
-        <Icon as={IoPencilOutline} color='gray.400' h={6} mx={1} w={6} />
-        <Icon
-          as={IoCloseCircleOutline}
-          color='gray.400'
-          cursor='pointer'
-          h={6}
-          ml={1}
-          w={6}
-          onClick={onDelete}
-        />
-      </Flex>
-    </Flex>
-  </ListItem>
-);
+    </ListItem>
+  );
+};
 
 export default ToDoListItem;
