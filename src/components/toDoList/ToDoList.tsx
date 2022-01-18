54852c0adc5443e9cbc728f8406d28ed521ebf86
Sebastian Vuye <sebastian.vuye@gmail.com>
2022-01-18 23:28:39 +0100

    feat(ToDoList): add empty state

--- src/components/toDoList/ToDoList.tsx
+++ src/components/toDoList/ToDoList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Grid, GridItem, List } from '@chakra-ui/react';
+import { Fade, Flex, Grid, GridItem, List, Text } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
 import ToDoListStats from './ToDoListStats';
 import ToDoListItem from './toDoListItem/ToDoListItem';
@@ -10,6 +10,7 @@ import * as ACTIONS from '../../store/actions';
 import { toDoListActionsLoadingSelector, toDoStateSelector } from '../../store/selectors';
 import { LOADING_DELAYS, TO_DO_LOADING_KEYS } from '../../constants';
 import { IToDoItem } from '../../types';
+import { ReactComponent as EmptyStateImage } from '../../assets/images/EmptyState.svg';
 
 const ToDoList = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -55,6 +56,23 @@ const ToDoList = (): JSX.Element => {
       <GridItem bg='teal.50' p={6}>
         <Grid h='100%' mx='auto' templateRows='1fr auto' w='90%'>
           <GridItem>
+            {toDoList.length === 0 && !isToDoActionLoading && (
+              <Flex alignItems='center' flexDir='column' h='100%' justifyContent='center'>
+                <Fade in transition={{ enter: { duration: 2 } }}>
+                  <Flex alignItems='center' flexDir='column' h='100%' justifyContent='center'>
+                    <Flex justifyContent='center' w='100%'>
+                      <EmptyStateImage height='100%' width='100%' />
+                    </Flex>
+                    <Flex alignItems='center' flexDir='column' mt={6}>
+                      <Text fontSize='2xl' fontWeight={700}>
+                        Add a To Do
+                      </Text>
+                      <Text color='gray.400'>What needs to be done?</Text>
+                    </Flex>
+                  </Flex>
+                </Fade>
+              </Flex>
+            )}
             <List spacing={4}>
               {toDoList.map((toDoListItem: IToDoItem) => (
                 <ToDoListItem
