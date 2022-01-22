2f261704bcaadbf4812e71e320da81e2ca68623c
Sebastian Vuye <sebastian.vuye@gmail.com>
2022-01-22 21:09:28 +0100

    refactor(footer&header): remove header & footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
 import { useDispatch } from 'react-redux';
-import { ErrorToast, Footer, Header, SuccessToast, ToDoList } from './components';
+import { ErrorToast, SuccessToast, ToDoList } from './components';
 import * as ACTIONS from './store/actions';
 
 function App(): JSX.Element {
@@ -17,15 +17,9 @@ function App(): JSX.Element {
       <SuccessToast />
       <main>
         <Grid bg='teal.900' h='100vh'>
-          <Grid mx='auto' templateRows='auto 1fr auto' w='90%'>
-            <GridItem bg='teal.900' p={4} textAlign='center'>
-              <Header />
-            </GridItem>
+          <Grid mx='auto' templateRows='1fr' w='100%'>
             <GridItem>
               <ToDoList />
-            </GridItem>
-            <GridItem p={6} textAlign='center'>
-              <Footer />
             </GridItem>
           </Grid>
         </Grid>
